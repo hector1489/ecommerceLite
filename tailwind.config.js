043b8bff3bef02b39c7427207eafc5f2e8129103
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-27 09:44:55 -0400

    technologies

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+  ],
+  theme: ["autumn"],
+}
+
